090d40e8a78e5d1053a0024de5a51e79ce73720e
Anik7303 <anik7303@gmail.com>
2021-01-23 22:29:30 +0600

    added linting - eslint

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+// routes
+const routes = require("./routes");
+
 const app = express();
 
 app.use(helmet());
@@ -9,5 +12,7 @@ app.use(morgan("combined"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, () => console.log("server listening on port 3000"));
